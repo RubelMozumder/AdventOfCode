c05325ba00cc93bdc881a4c5dc03855cb3747b5a
RubelMozumder <rubel.mozumder@outlook.com>
2025-12-07 00:35:50 +0100

    Part1 and part2 both are done.

--- 2025/Python/day-1/test.py
+++ 2025/Python/day-1/test.py
@@ -1,22 +1,26 @@
-from secret_entrance import walk_left, walk_right
-from main import main
+from secret_entrance import walk_left1, walk_right1, find_password_part1, find_password_part2
 from pathlib import Path
 
 if __name__ == "__main__":
-    assert walk_left(50, 68) == (82, 0)
-    assert walk_left(82, 30) == (52, 0)
-    assert walk_right(52, 48) == (0, 1), walk_right(52, 48)
-    assert walk_left(0, 5) == (95, 0)
-    assert walk_right(95, 60) == (55, 0)
-    assert walk_left(55, 55) == (0, 1)
-    assert walk_left(0, 1) == (99, 0)
-    assert walk_left(99, 99) == (0, 1)
-    assert walk_right(0, 14) == (14, 0)
-    assert walk_left(14, 82) == (32, 0)
-    assert walk_right(50, 50) == (0, 1)
+    assert walk_left1(50, 68) == (82, 0)
+    assert walk_left1(82, 30) == (52, 0)
+    assert walk_right1(52, 48) == (0, 1)
+    assert walk_left1(0, 5) == (95, 0)
+    assert walk_right1(95, 60) == (55, 0)
+    assert walk_left1(55, 55) == (0, 1)
+    assert walk_left1(0, 1) == (99, 0)
+    assert walk_left1(99, 99) == (0, 1)
+    assert walk_right1(0, 14) == (14, 0)
+    assert walk_left1(14, 82) == (32, 0)
+    assert walk_right1(50, 50) == (0, 1)
 
     current_dir = Path(__file__).resolve().parent
-    puzzel_input_file1 = current_dir / "test.txt"
-    puzzel_input_file2 = current_dir / "puzzle_input.txt"
-    assert main(puzzel_input_file=puzzel_input_file1) == 3
-    assert main(puzzel_input_file=puzzel_input_file2) == 1089
+    puzzle_input_file1 = current_dir / "test1.txt"
+    puzzle_input_file2 = current_dir / "puzzle_input.txt"
+    assert find_password_part1(puzzle_input_file=puzzle_input_file1, start_point=50) == 3
+    assert find_password_part1(puzzle_input_file=puzzle_input_file2, start_point=50) == 1089
+    puzzle_input_file1 = current_dir / "test2.txt"
+    assert find_password_part2(puzzle_input_file=puzzle_input_file1, start_point=50) == 6
+    assert (
+        find_password_part2(puzzle_input_file=puzzle_input_file2, start_point=50) == 6530
+    )
