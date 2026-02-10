c86e6f27c998322c7920e574fa7e0f3fb7a5fffa
RubelMozumder <rubel.mozumder@outlook.com>
2026-02-10 22:46:00 +0100

    Summation of the problem members are not done.

--- 2025/js/day-6/index.js
+++ 2025/js/day-6/index.js
@@ -7,7 +7,7 @@ import {
 const mathSheetExample = `123 328  51 64 
  45 64  387 23 
   6 98  215 314
-*   +   *   + 
+*   +   *   +  
 `;
 
 const sumOfInnerArray = arrayOfInnerReducedArrPart1(mathSheetExample);
--- 2025/js/day-6/modules/cepaloposMathProblem.js
+++ 2025/js/day-6/modules/cepaloposMathProblem.js
@@ -21,28 +21,57 @@ function createMap(lastLine) {
 
 export function arrayOfInnerReducedArrPart2(input_text) {
   const lineArray = getLineArray(input_text);
-  const twoDArray = Array(lineArray.length);
+
+  const endLine = lineArray.pop();
+
+  const { strToOpt, strToStartingValue, mathSymbols } = createMap(endLine);
+  const numberIndices = [];
+
+  for (let ind = 1; ind < endLine.length; ind++) {
+    if (Object.keys(strToOpt).includes(endLine.at(ind))) {
+      numberIndices.push(ind - 2);
+    }
+  }
+  numberIndices.push(endLine.length - 1);
+
+  const twoDArray = Array(numberIndices.length)
+    .fill(null)
+    .map(() => []);
 
   for (let row_i = 0; row_i < lineArray.length; row_i++) {
     const line = lineArray[row_i];
-    let char_i = 0;
     let col_i = 0;
 
     let number = '';
-    let pre_char = '';
-    for (let char of line) {
-      if (pre_char !== '' && char == ' ') {
-        const col_arr = twoDArray[col_i];
-        col_i++;
-        col_arr.push(number);
+    for (let char_i = 0; char_i < line.length; char_i++) {
+      number += line.at(char_i);
+      if (numberIndices[col_i] == char_i) {
+        twoDArray[col_i].push(number);
         number = '';
-      } else {
-        number += char;
+        col_i++;
       }
-      char_i++;
+
+      // console.log('twoDArray : ', twoDArray);
+      // if (char_i == line.length - 1) {
+      // number += char;
+      // twoDArray[char_i].push(number);
+      // continue;
+      // }
+      // if (pre_char !== ' ' && char == ' ') {
+      // const col_arr = twoDArray[col_i];
+      // console.log(' col_arr : ', col_i);
+      // twoDArray[col_i].push(number);
+      // number = '';
+      // col_i++;
+      // pre_char = char;
+      // } else {
+      // number += char;
+      // pre_char = char;
+      // }
+      // char_i++;
     }
   }
-
+  console.log(' Two-D array : ', twoDArray);
   twoDArray.forEach((col, ind, twoDArray) => {
     twoDArray[ind] = col.map((elm) => elm.split(''));
   });
@@ -50,18 +79,16 @@ export function arrayOfInnerReducedArrPart2(input_text) {
   const reducedTwoArray = Array(twoDArray.length);
   for (let col_ind = 0; col_ind < twoDArray.length; col_ind++) {
     const col = twoDArray[col_ind];
-    console.log(' col ', col);
-    const sumInnerElm = Array(col.length).map((x) => '');
-    for (let inner_elm = 0; inner_elm < col.length; inner_elm++) {
-      inner_elm.forEach((end_elm, ind) => {
+    const sumInnerElm = [];
+    for (let row_ind = 0; row_ind < col.length; row_ind++) {
+      col[row_ind].forEach((end_elm, ind) => {
         sumInnerElm[ind] += end_elm;
       });
     }
+    console.log(sumInnerElm);
     reducedTwoArray[col_ind] = sumInnerElm;
   }
-  const { strToOpt, strToStartingValue, mathSymbols } = createMap(
-    lineArray.pop(),
-  );
+
   reducedTwoArray.forEach((col, ind) => {
     reducedTwoArray[ind] = col
       .map((x) => x.trim() * 1)
